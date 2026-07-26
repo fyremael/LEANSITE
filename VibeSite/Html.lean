3bfc80b53a0991d7b2e261a3ee4144496d454773
Fyre Mael <fyremaelstrom@gmail.com>
2026-07-26 03:51:45 -0700

    Avoid reserved attribute identifier

--- VibeSite/Html.lean
+++ VibeSite/Html.lean
@@ -9,14 +9,14 @@ inductive Html where
 
 namespace Html
 
-private def escapeWith (attribute : Bool) (input : String) : String :=
+private def escapeWith (isAttribute : Bool) (input : String) : String :=
   input.toList.foldl (fun out c =>
     match c with
     | '&' => out ++ "&amp;"
     | '<' => out ++ "&lt;"
     | '>' => out ++ "&gt;"
-    | '"' => if attribute then out ++ "&quot;" else out.push c
-    | '\'' => if attribute then out ++ "&#39;" else out.push c
+    | '"' => if isAttribute then out ++ "&quot;" else out.push c
+    | '\'' => if isAttribute then out ++ "&#39;" else out.push c
     | _ => out.push c) ""
 
 def escapeText (input : String) : String :=
@@ -25,8 +25,8 @@ def escapeText (input : String) : String :=
 def escapeAttribute (input : String) : String :=
   escapeWith true input
 
-private def renderAttribute (attribute : String × String) : String :=
-  s!" {attribute.1}=\"{escapeAttribute attribute.2}\""
+private def renderAttribute (attr : String × String) : String :=
+  s!" {attr.1}=\"{escapeAttribute attr.2}\""
 
 private def isVoidElement (tag : String) : Bool :=
   ["area", "base", "br", "col", "embed", "hr", "img", "input", "link", "meta", "source", "track", "wbr"].contains tag
